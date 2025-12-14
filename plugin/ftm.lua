909c00fe8e778bde172c299aeb300819a6923be3
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2025-12-14 01:45:18 +0100

    feat: add parameterized Ftm command with cmdparse
    Refactored the Ftm user command to use mega.cmdparse's ParameterParser,
    enabling named and optional command parameters for better management of
    multiple terminals. The new implementation provides help descriptions
    and improved flexibility for terminal invocation.

--- plugin/ftm.lua
+++ plugin/ftm.lua
@@ -1,1 +1,17 @@
-vim.api.nvim_create_user_command('FTMToggle', require('ftm').get_or_create(), {})
+local cmdparse = require('mega.cmdparse')
+local parser = cmdparse.ParameterParser.new({ name = 'Ftm', help = 'Unicode Parameters.' })
+
+parser:add_parameter({
+  name = 'name',
+  help = 'Name you can assign this terminal. This helps with managment of multiple terminals (required).',
+})
+parser:add_parameter({
+  name = 'cmd',
+  required = false,
+  help = 'Command you can run in this terminal (optional).',
+})
+parser:set_execute(function(data)
+  require('ftm').toggle({ name = data.namespace.name, cmd = data.namespace.cmd })
+end)
+
+cmdparse.create_user_command(parser)
