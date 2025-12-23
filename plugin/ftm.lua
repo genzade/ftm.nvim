7a83e08a8acce4087d148f8933ff0e3b763edbce
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2025-12-24 00:34:43 +0100

    refactor(plugin): remove cmdparse dependency from ftm
    Removed the usage of the 'mega.cmdparse' module and related parameter
    parsing logic from the ftm plugin entrypoint. This simplifies the plugin
    by eliminating an external dependency and its associated user command
    registration, focusing on core functionality.

--- plugin/ftm.lua
+++ plugin/ftm.lua
@@ -1,20 +1,5 @@
-local cmdparse = require('mega.cmdparse')
-local parser = cmdparse.ParameterParser.new({ name = 'Ftm', help = 'Unicode Parameters.' })
 
-parser:add_parameter({
-  name = 'name',
-  help = 'Assign a name to this terminal. This helps with managment of multiple terminals (required).',
-})
-parser:add_parameter({
-  name = 'cmd',
-  required = false,
-  help = 'Command you can run in this terminal (optional).',
-})
-parser:set_execute(function(data)
-  require('ftm').toggle({ name = data.namespace.name, cmd = data.namespace.cmd })
-end)
 
-cmdparse.create_user_command(parser)
 
 vim.api.nvim_create_autocmd('VimResized', {
   group = vim.api.nvim_create_augroup('FTM_ResizeAll', { clear = true }),
